4e369d22c4352aa8df255870dcfd420c883330c9
sergiohscl <sergiohscl2@gmail.com>
2021-11-29 17:04:18 -0300

    Exercio Bloco 01

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,34 @@
+//let a = 10
+// b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+//Resposta 10 5
+
+//let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b  
+
+// console.log(a, b, c)
+// Resposta 10 10 10
+
+/*let horasTrabalhada = prompt("Quantas horas voce trabalha por dia?")
+let salarioDia = prompt("Quanto voce recebe por dia?")
+alert(`Voce recebe ${horasTrabalhada/salarioDia} por hora`)*/
+
+//EXERCICIOS DE ESCRITA DE CÓDIGO
+
+const nomeUsuario = "segio"
+const idadeUsuario = 49
+console.log(typeof nomeUsuario, typeof idadeUsuario)
+
+
+//const nomeUsuario = prompt("Qual o seu nome?")
+// const idadeUsuario = prompt("Qual a sua idade?")
+
+// console.log(typeof nomeUsuario, typeof idadeUsuario)
+
