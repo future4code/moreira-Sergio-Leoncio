46ffe99d5affcc51313fa594e58e5be63364b43b
Sergio <sergiohscl2@gmail.com>
2022-03-22 17:51:52 -0300

    primeira aula de back-end

---
+++ modulo4/node-package-json/src/index.js
@@ -0,0 +1,11 @@
+// const nome = (process.argv[2])
+// const idade  = Number(process.argv[3])
+// const dados = `Ola, ${nome}! Você tem ${idade + 7} anos`
+
+// console.log(dados)
+
+const num1 = Number(process.argv[2])
+const num2 = Number(process.argv[3])
+const valores = num1 * num2
+console.log(valores)
+
