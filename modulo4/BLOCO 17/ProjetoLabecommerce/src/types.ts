0028c9b25c947f498b525f0d3adf6dea6357dfde
Sergio <sergiohscl2@gmail.com>
2022-04-15 13:18:39 -0300

    ProjetoLabecommerce em andamento

---
+++ modulo4/BLOCO 17/ProjetoLabecommerce/src/types.ts
@@ -0,0 +1,13 @@
+export type user = {
+   id: number
+   name: string
+   email: string
+   password: string
+}
+
+export type products = {
+   id: number;
+   name: string;
+   price: number;
+   imagem: string
+}
