7d05d06af2f15653864e7f7624f4878247e3d277
Sergio <sergiohscl2@gmail.com>
2022-05-10 16:52:28 -0300

    aula testes-unitarios-mock

---
+++ modulo5/BLOCO 21/testes-unitarios-mocks/src/performAttack.ts
@@ -0,0 +1,17 @@
+import { validateCharacter } from "./validateCharacter";
+import { Character } from "./models/Character";
+  
+export const performAttack = (
+    attacker: Character,
+    defender: Character,
+    validator: (input: Character) => boolean
+  ) => {
+    if (!validator(attacker) || !validator(defender)) {
+      throw new Error("Invalid character");
+    }
+  
+    if (attacker.strength > defender.defense) {
+      defender.life -= attacker.strength - defender.defense;
+    }
+  };
+
